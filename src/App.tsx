904d488cd4f7e0059266f9224e1dce663ca39b9c
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-05-20 08:40:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+
+const App: React.FunctionComponent = () => (
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+);
+
+export default App;
