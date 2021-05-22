460d314e82b93d8928072a97f660ea78f6233e3d
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-05-22 16:24:46 -0300

    feat: add style to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import logoImg from "../../assets/github-logo.svg";
+import { Title, Form } from "./styles";
 
-const Dashboard: React.FunctionComponent = () =>{
-    return <h1>Dashboard</h1>;
+const Dashboard: React.FunctionComponent = () => {
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+        </>
+    );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    font-size: 48px;
+    color: #3a3a3a;
+    max-width: 450px;
+    line-height: 56px;
+
+    margin-top: 80px;
+`;
+
+export const Form = styled.form`
+    margin-top: 40px;
+    max-width: 700px;
+    display: flex;
+
+    input{
+        flex: 1;
+        height: 70px;
+        padding: 0 24px;
+        border: 0;
+        border-radius: 4px;
+
+
+    }
+
+    button{
+        color: #fff;
+        font-size: bold;
+        width: 210px;
+        height: 70px;
+        border-radius: 4px;
+        background: #287ae0;
+        border: none;
+        margin-left: 10px;
+
+        button:hover{
+            background-color: 
+        }
+
+    }
+`;
