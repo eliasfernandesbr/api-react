66fb47e57455cec54a118ba2b93c56f5ce816dbb
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-05-22 21:09:54 -0300

    feat: add axios and api connection

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,81 +1,65 @@
-import React from "react";
-import { FiChevronRight } from "react-icons/fi";
+import React, { useState, FormEvent } from "react";
+import api from "../../services/api";
 import logoImg from "../../assets/github-logo.svg";
 import arrowImg from "../../assets/arrow.svg";
 import { Title, Form, Repositories, ArrowIcon } from "./styles";
 
+//TIPAGEM PARA O TYPESCRIPT \/
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FunctionComponent = () => {
+    const [newRepo, setNewRepo] = useState("");
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>
+    ): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get(`repos/${newRepo}`);
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+        // Adição de um novo repositório
+        // Consumir API do github e depois salvar o novo repositória no estado de rep;
+    }
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
-                    <img
-                        className="imgAvatar"
-                        src="https://avatars.githubusercontent.com/u/41518685?v=4"
-                        alt="Elias Fernandes"
-                    />
-                    <div>
-                        <strong>eliasfernandesbr/api-react</strong>
-                        <p>react api para pesquisar repositórios no github</p>
-                    </div>
-                    <ArrowIcon src={arrowImg} />
-                </a>
-                <a href="teste">
-                    <img
-                        className="imgAvatar"
-                        src="https://avatars.githubusercontent.com/u/41518685?v=4"
-                        alt="Elias Fernandes"
-                    />
-                    <div>
-                        <strong>eliasfernandesbr/api-react</strong>
-                        <p>react api para pesquisar repositórios no github</p>
-                    </div>
-                    <ArrowIcon src={arrowImg} />
-                </a>
-                <a href="teste">
-                    <img
-                        className="imgAvatar"
-                        src="https://avatars.githubusercontent.com/u/41518685?v=4"
-                        alt="Elias Fernandes"
-                    />
-                    <div>
-                        <strong>eliasfernandesbr/api-react</strong>
-                        <p>react api para pesquisar repositórios no github</p>
-                    </div>
-                    <ArrowIcon src={arrowImg} />
-                </a>
-                <a href="teste">
-                    <img
-                        className="imgAvatar"
-                        src="https://avatars.githubusercontent.com/u/41518685?v=4"
-                        alt="Elias Fernandes"
-                    />
-                    <div>
-                        <strong>eliasfernandesbr/api-react</strong>
-                        <p>react api para pesquisar repositórios no github</p>
-                    </div>
-                    <ArrowIcon src={arrowImg} />
-                </a>
-                <a href="teste">
-                    <img
-                        className="imgAvatar"
-                        src="https://avatars.githubusercontent.com/u/41518685?v=4"
-                        alt="Elias Fernandes"
-                    />
-                    <div>
-                        <strong>eliasfernandesbr/api-react</strong>
-                        <p>react api para pesquisar repositórios no github</p>
-                    </div>
-                    <ArrowIcon src={arrowImg} />
-                </a>
+                {repositories.map((repository) => (
+                    <a key={repository.full_name} href="teste">
+                        <img
+                            className="imgAvatar"
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <ArrowIcon src={arrowImg} />
+                    </a>
+                ))}
             </Repositories>
         </>
     );
